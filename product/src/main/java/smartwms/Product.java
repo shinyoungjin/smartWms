99a7a9ffc49f22f0729321134ae618465e493c08
shinyoungjin <77368724+shinyoungjin@users.noreply.github.com>
2021-02-15 22:05:26 +0900

    Add files via upload

---
+++ product/src/main/java/smartwms/Product.java
@@ -0,0 +1,81 @@
+package smartwms;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Product_table")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String productId;
+    private String productName;
+    private String productStatus;
+    private String createDate;
+    private String updateDate;
+
+    @PostPersist
+    public void onPostPersist(){
+        ProductRegistered productRegistered = new ProductRegistered();
+        BeanUtils.copyProperties(this, productRegistered);
+        productRegistered.publishAfterCommit();
+
+
+        ProductChanged productChanged = new ProductChanged();
+        BeanUtils.copyProperties(this, productChanged);
+        productChanged.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
+
+
+
+}
---
+++ product/src/main/java/smartwms/ProductChanged.java
@@ -0,0 +1,58 @@
+package SMARTWMS;
+
+public class ProductChanged extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private String productName;
+    private String productStatus;
+    private String createDate;
+    private String updateDate;
+
+    public ProductChanged(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+}
