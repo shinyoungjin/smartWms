094bd44296e95ca4105654dec7d939d7b1b5d464
shinyoungjin <77368724+shinyoungjin@users.noreply.github.com>
2021-02-15 22:04:54 +0900

    Add files via upload

---
+++ order/src/main/java/smartwms/Order.java
@@ -0,0 +1,101 @@
+package smartwms;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Order_table")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String orderId;
+    private String productId;
+    private String productName;
+    private String orderStatus;
+    private Integer orderQty;
+    private String deliveryOrderStatus;
+
+    @PostPersist
+    public void onPostPersist(){
+        Ordered ordered = new Ordered();
+        BeanUtils.copyProperties(this, ordered);
+        ordered.publishAfterCommit();
+
+
+    }
+
+    @PreRemove
+    public void onPreRemove(){
+        OrderCanceled orderCanceled = new OrderCanceled();
+        BeanUtils.copyProperties(this, orderCanceled);
+        orderCanceled.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        .external.DeliveryOrder deliveryOrder = new .external.DeliveryOrder();
+        // mappings goes here
+        Application.applicationContext.getBean(.external.DeliveryOrderService.class)
+            .deliveryCancel(deliveryOrder);
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+    public Integer getOrderQty() {
+        return orderQty;
+    }
+
+    public void setOrderQty(Integer orderQty) {
+        this.orderQty = orderQty;
+    }
+    public String getDeliveryOrderStatus() {
+        return deliveryOrderStatus;
+    }
+
+    public void setDeliveryOrderStatus(String deliveryOrderStatus) {
+        this.deliveryOrderStatus = deliveryOrderStatus;
+    }
+
+
+
+
+}
---
+++ order/src/main/java/smartwms/external/DeliveryOrderService.java
@@ -0,0 +1,17 @@
+
+package smartwms.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="deliveryOrder", url="http://deliveryOrder:8080")
+public interface DeliveryOrderService {
+
+    @RequestMapping(method= RequestMethod.POST, path="/deliveryOrders")
+    public void deliveryCancel(@RequestBody DeliveryOrder deliveryOrder);
+
+}
