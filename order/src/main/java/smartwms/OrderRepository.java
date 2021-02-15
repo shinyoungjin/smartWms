592481b72fa97a1c87f20d9e8f66b9f824471532
shinyoungjin <77368724+shinyoungjin@users.noreply.github.com>
2021-02-15 21:57:22 +0900

    Delete order directory

--- order/src/main/java/smartwms/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package smartwms;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
-
-
-}
