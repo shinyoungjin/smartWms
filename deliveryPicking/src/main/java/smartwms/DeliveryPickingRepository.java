0ea9cc21b0c23209d8f6d9f06be2c777a1863871
shinyoungjin <77368724+shinyoungjin@users.noreply.github.com>
2021-02-15 21:56:57 +0900

    Delete deliveryPicking directory

--- deliveryPicking/src/main/java/smartwms/DeliveryPickingRepository.java
+++
@@ -1,8 +0,0 @@
-package smartwms;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DeliveryPickingRepository extends PagingAndSortingRepository<DeliveryPicking, Long>{
-
-
-}
